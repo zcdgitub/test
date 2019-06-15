512221ebf2cac153739b387acbbba934585e3392
zcdgithub <15538110796@163.com>
2019-06-16 00:12:03 +0800

    zc2

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="hello">
+
+    <h2>Essential Links</h2>
+ <input type="text" placeholder="请输入手机" v-model="name">
+          <input type="password" placeholder="请输入密码" v-model="password">
+
+    <ul>
+      <li>
+        <a
+         
+         @click="reg"
+        >
+          注册
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+        >
+          Forum
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+        >
+          Community Chat
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a
+          href="http://router.vuejs.org/"
+          target="_blank"
+        >
+          vue-router
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vuex.vuejs.org/"
+          target="_blank"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vue-loader.vuejs.org/"
+          target="_blank"
+        >
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+        >
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+       name: '',
+       password: '',
+    }
+  },
+   methods:{
+      reg(){
+
+          let $this=this;//用变量存储父函数 以便在子函数使用
+         //this.$axios.post('/api/user/login',this.$qs.stringify({
+         //this.$axios.post('/api/store/saveImage',this.$qs.stringify({
+          //name:this.name,
+          //password:this.password,
+         // api_token:sessionStorage.getItem('api_token')
+         //})
+      this.$axios.get('http://www.lar.com/index.php/api/goods/index'
+     //  this.$axios.get('http://www.lar.com/index.php/api/goodsCate/index'
+       //this.$axios.get('/api/goods/detail',{params:{api_token:sessionStorage.getItem('api_token')}}
+       // this.$axios.get('/api/store/saveImage',{params:{api_token:sessionStorage.getItem('api_token')}}
+       //this.$axios.get('/api/store/index',{params:{api_token:sessionStorage.getItem('api_token')}}
+      // this.$axios.get('/api/index/index',{params:{api_token:sessionStorage.getItem('api_token')}}
+           ).then(function(response){
+           console.log(response);
+           // console.log(response.data.data.api_token);
+          //  sessionStorage.setItem('api_token',response.data.data.api_token);
+            $this.$layer.msg(response.data.msg);
+         })
+      }
+   }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from 'axios'
+import qs from 'qs'
+import  layer from 'vue-layer'
+Vue.prototype.$axios=axios//全局配置，不用每次用import引入
+Vue.prototype.$qs=qs
+Vue.prototype.$layer=layer(Vue)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
